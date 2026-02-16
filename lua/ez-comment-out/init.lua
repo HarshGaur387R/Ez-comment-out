c77d59664350623f573578a6beee1b32d182f56f
Harsh Gaur <hgaur701@gmail.com>
2026-02-16 23:07:49 +0530

    ADDED: gitignore

--- lua/ez-comment-out/init.lua
+++ lua/ez-comment-out/init.lua
@@ -1,4 +1,20 @@
+local Debug_log = require("ez-comment-out.Debug_log")
 local M = {}
+
+--- This function returns the file type
+--- @return string | nil
+function GetFileType()
+	local buf = vim.api.nvim_get_current_buf();
+	local ft = vim.bo[buf].filetype
+	local result = ft or nil
+	return result
+end
+
+--- This function handle <ctrl + _> command
+local function handle_command()
+	print(GetFileType())
+	Debug_log(GetFileType() or "")
+end
 
 --- Config the plugin
 ---@param opts table
@@ -6,7 +22,7 @@ function M.setup(opts)
 	opts = opts or {}
 
 	vim.keymap.set({ 'v', 'n' }, '<C-_>', function()
-		print("testing!")
+		handle_command()
 	end, { desc = "command to comment out selected lines" })
 end
 
