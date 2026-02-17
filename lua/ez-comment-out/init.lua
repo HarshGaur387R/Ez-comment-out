ec336f980fb387c2d66b9122c82b8ef01e0b6e59
Harsh Gaur <hgaur701@gmail.com>
2026-02-17 18:33:21 +0530

    UPDATED: Wrote saparate functions to handle both mode separately

--- lua/ez-comment-out/init.lua
+++ lua/ez-comment-out/init.lua
@@ -23,16 +23,67 @@ function ReadJsonFile(path)
 end
 
 --- This function decods json data
+--- @param content any
+--- @return any
 function DecodeJsonData(content)
 	local data = vim.json.decode(content)
 	return data
 end
 
---- This function handle <ctrl + _> command
-local function handle_command()
+--- This function sets comment symbol before the current line
+---@param symbol string
+---@param line string
+---@return string
+local function insert_single_line_comment(symbol, line)
+	local result = symbol .. " " .. line
+	return result
+end
+
+
+--- This function append and prepend symbols at the given line
+---@param starting_symbol string
+---@param ending_symbol string
+---@param lines table
+---@return table
+local function insert_multi_line_comment(starting_symbol, ending_symbol, lines)
+	local result = {}
+	-- TODO : complete this
+
+	return result
+end
+
+--- This function handle <ctrl + _> command for normal mode
+--- @param content any
+local function handle_command_for_normal_mode(content)
 	local fileType = GetFileType()
-	local content = ReadJsonFile("comments_symbols.json")
-	local decodedContent = DecodeJsonData(content)
+	local temp = content.languages
+	local current_line = vim.api.nvim_get_current_line()
+	local comment_symbol = temp[fileType]
+
+	if not comment_symbol then return end
+
+	if comment_symbol["single_line"] then
+		local result = insert_single_line_comment(comment_symbol["single_line"], current_line)
+		vim.api.nvim_set_current_line(result)
+		return
+	elseif comment_symbol["multi_line"] then
+		local starting_symbol, ending_symbol =
+		    comment_symbol["multi_line"]["start"],
+		    comment_symbol["multi_line"]["end"]
+
+		local result = insert_multi_line_comment(
+			starting_symbol,
+			ending_symbol,
+			{ current_line }
+		)
+
+		vim.api.nvim_set_current_line(result[1])
+	end
+end
+
+local function handle_command_for_visual_mode(content)
+	local fileType = GetFileType()
+	local temp = content.languages
 end
 
 --- Config the plugin
@@ -40,9 +91,16 @@ end
 function M.setup(opts)
 	opts = opts or {}
 
-	vim.keymap.set({ 'v', 'n' }, '<C-_>', function()
-		handle_command()
-	end, { desc = "command to comment out selected lines" })
+	local content = ReadJsonFile("comments_symbols.json")
+	local decodedContent = DecodeJsonData(content)
+
+	vim.keymap.set({ 'n' }, '<C-_>', function()
+		handle_command_for_normal_mode(decodedContent)
+	end, { desc = "command to comment out single line normal mode" })
+
+	vim.keymap.set({ "x" }, '<C-_>', function()
+		handle_command_for_visual_mode(decodedContent)
+	end, { desc = "command to comment out selected line" })
 end
 
 return M
