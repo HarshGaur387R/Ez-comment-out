55c0ea734e2033d9c98860c225cd88bd7d5f49a0
Harsh Gaur <hgaur701@gmail.com>
2026-02-19 22:50:42 +0530

    UPDATED: Added filetype security check.

--- lua/ez-comment-out/init.lua
+++ lua/ez-comment-out/init.lua
@@ -11,12 +11,14 @@ function M.setup(opts)
 
 	vim.keymap.set({ 'n' }, '<C-_>', function()
 		local fileType = utils.GetFileType()
+		if not fileType then return end
 		comments.handle_command_for_normal_mode(fileType, decodedContent)
 	end, { desc = "command to comment out single line normal mode" })
 
 	vim.keymap.set({ "x" }, '<C-_>', function()
 		vim.cmd('normal! \27') -- or use feedkeys
 		local fileType = utils.GetFileType()
+		if not fileType then return end
 		comments.handle_command_for_visual_mode(fileType, decodedContent)
 	end, { desc = "command to comment out selected line" })
 end
